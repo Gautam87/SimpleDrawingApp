d22c14dbc192f380ebf19bb658b224ed14caee07
Gautam Sukhramani <gautam@medikoe.com>
2018-07-09 20:53:05 +0530

    initial commit

---
+++ app/src/main/java/draw/blazon/com/simpledrawingapp/DrawingActivity.java
@@ -0,0 +1,14 @@
+package draw.blazon.com.simpledrawingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawing);
+    }
+
+}
